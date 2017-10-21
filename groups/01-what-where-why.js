cd25730e3fe4b96cdb30c3348156d83c60bb1c87
Elijah Manor <elijah.manor@gmail.com>
2017-10-20 22:21:52 -0500

    Add group slides

---
+++ groups/01-what-where-why.js
@@ -0,0 +1,33 @@
+import React from "react";
+import {
+  BlockQuote,
+  Cite,
+  Heading,
+  ListItem,
+  List,
+  Quote,
+  Slide,
+  Text,
+  CodePane,
+  ComponentPlayground,
+  Fill,
+  Layout,
+  MarkdownSlides,
+  Link,
+  Markdown,
+  Appear,
+  Image
+} from "spectacle";
+
+export default (theme, images) => [
+  <Slide
+    id="what-where-why"
+    transition={["slide"]}
+    bgColor="black"
+    notes="You can even put notes on your slide. How awesome is that?"
+  >
+    <Heading textColor="quartenary" caps fit>
+      What is React?
+    </Heading>
+  </Slide>
+];
---
+++ groups/16-universal-javascript.js
@@ -0,0 +1,33 @@
+import React from "react";
+import {
+  BlockQuote,
+  Cite,
+  Heading,
+  ListItem,
+  List,
+  Quote,
+  Slide,
+  Text,
+  CodePane,
+  ComponentPlayground,
+  Fill,
+  Layout,
+  MarkdownSlides,
+  Link,
+  Markdown,
+  Appear,
+  Image
+} from "spectacle";
+
+export default (theme, images) => [
+  <Slide
+    id="universal-javascript"
+    transition={["slide"]}
+    bgColor="black"
+    notes="You can even put notes on your slide. How awesome is that?"
+  >
+    <Heading textColor="quartenary" caps fit>
+      Universal JavaScript
+    </Heading>
+  </Slide>
+];
