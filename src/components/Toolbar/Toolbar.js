dbe75111dce849f96e0136ee4d6567f7570fdc62
Elijah Manor <elijah.manor@gmail.com>
2017-10-25 00:00:38 -0500

    Add logo text and flame animated css

--- src/components/Toolbar/Toolbar.js
+++ src/components/Toolbar/Toolbar.js
@@ -9,8 +9,15 @@ const Toolbar = ({ className, user, onNew }) => {
   const classes = classNames("Toolbar", className);
   return (
     <ul className={classes}>
-      <li className="Toolbar-item">
-        <img className="Toolbar-logo" src={logo} />
+      <li className="Toolbar-item Toolbar-item--logo">
+        <span class="fire">o</span>
+        <span class="burn">v</span>
+        <span class="burn">e</span>
+        <span class="fire">r</span>
+        <span class="fire">e</span>
+        <span class="burn">a</span>
+        <span class="burn">c</span>
+        <span class="fire">t</span>
       </li>
       <li className="Toolbar-item" onClick={onNew}>
         <Button>Fuse</Button>
