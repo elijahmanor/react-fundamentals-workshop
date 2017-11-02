5ecad8b3f94217c76f07512a2e431e55fa7cc5c6
Elijah Manor <elijah.manor@gmail.com>
2017-11-01 23:31:52 -0700

    Add Fuses and update slides to show only what is needed

---
+++ src/components/Fuses/Fuses.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Fuses = props => {
+  return <div>FUSES</div>;
+};
+
+export default Fuses;
---
+++ src/components/Fuses/Fuses.test.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { shallow, mount } from "enzyme";
+import ReactDOM from "react-dom";
+import renderer from "react-test-renderer";
+import Fuse from "../Fuse/Fuse";
+import Fuses from "./Fuses";
+
+jest.mock("../Fuse/Fuse", () => "Fuse");
+
+describe("Fuses", () => {
+  let component;
+
+  const render = props => {
+    props = {
+      fuses: [
+        { id: "1", message: "fuse1" },
+        { id: "2", message: "fuse2" },
+        { id: "3", message: "fuse3" }
+      ]
+    };
+    return shallow(<Fuses {...props} />);
+  };
+
+  beforeEach(() => {
+    component = render();
+  });
+
+  it("should render 3 fuses", () => {
+    expect(component.find("Fuse").length).toBe(3);
+  });
+
+  it("should spread other values of each fuse to the Fuse component", () => {
+    component.find("Fuse").forEach((fuse, index) => {
+      expect(fuse.prop("message")).toBe(`fuse${index + 1}`);
+    });
+  });
+});
