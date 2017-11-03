8b136cfc6dc6e07993d8a2e8c5fc4194ad2294a5
Elijah Manor <elijah.manor@gmail.com>
2017-11-02 19:53:05 -0700

    add stuff to key

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,13 +1,45 @@
-import React from "react";
-import classNames from "classnames";
+import styled from "styled-components";
+import { darken, modularScale } from "polished";
 
-import "./Button.css";
+const SECTION_BG = "white";
+const BUTTON_COLOR = "#95c83e";
 
-const Button = props => {
-  const classes = classNames("Button", props.className, {
-    "is-disabled": props.disabled
-  });
-  return <button className={classes} {...props} />;
-};
+const Button = styled.button`
+  height: 30px;
+  min-width: 60px;
+  text-align: center;
+  font-size: 1.25rem;
+  font-weight: 600;
+  outline: none;
+  padding: 4px 15px;
+  border: none;
+  border-radius: 3px;
+  margin-right: 10px;
+  transition: background-color 200ms ease-in-out, opacity 200ms ease-in-out,
+    border-bottom 200ms ease-in-out;
+  vertical-align: middle;
+  background-color: ${BUTTON_COLOR};
+  color: ${SECTION_BG};
+  box-shadow: 0 2px ${darken(0.1, BUTTON_COLOR)};
+
+  &:last-of-type {
+    margin-right: 0;
+  }
+
+  cursor: ${props => (props.disabled ? "not-allowed" : "pointer")};
+  opacity: ${props => (props.disabled ? "0.5" : "1")};
+
+  &:hover,
+  &:focus {
+    background-color: ${darken(0.1, BUTTON_COLOR)};
+    box-shadow: 0 2px ${darken(0.2, BUTTON_COLOR)};
+  }
+
+  &:active {
+    background-color: ${darken(0.2, BUTTON_COLOR)};
+    box-shadow: 0 1px ${darken(0.3, BUTTON_COLOR)};
+    transform: translateY(1px);
+  }
+`;
 
 export default Button;
--- src/setupTests.js
+++ src/setupTests.js
@@ -1,6 +1,6 @@
 import raf from "./polyfills";
 import { configure } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
+import "jest-styled-components";
 
 configure({ adapter: new Adapter() });
-
